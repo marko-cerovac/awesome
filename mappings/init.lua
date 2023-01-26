20467bfe649633a9b65d53827c5149b86c3ef221
marko-cerovac <marko.cerovac16@gmail.com>
2023-01-26 03:55:04 +0100

    setting up the rewrite

---
+++ mappings/init.lua
@@ -0,0 +1,5 @@
+-- set default modkey
+modkey = "Mod4"
+
+-- define global mappings table
+globalkeys = {}
--- rc.lua
+++ rc.lua
@@ -1,65 +1,67 @@
--- If LuaRocks is installed, make sure that packages installed through it are
--- found (e.g. lgi). If LuaRocks is not installed, do nothing.
+-- check if luarocks and dependencies are installed
 pcall(require, "luarocks.loader")
 
--- Standard awesome library
-local awful = require("awful")
-require("awful.autofocus")
+-- import libraries:
+local gears         = require "gears"
+local awful         = require "awful"
+local wibox         = require "wibox"
+local beautiful     = require "beautiful"
+local naughty       = require "naughty"
+local menubar       = require "menubar"
+local hotkeys_popup = require "awful.hotkeys_popup"
+require "awful.autofocus"
 
--- Theme handling library
-local beautiful = require("beautiful")
+-- enable autofocus
+require "awful.autofocus"
 
--- Notification library
-local naughty = require("naughty")
-
-util_dir = "/home/marko/.config/awesome/util/"
-
--- Enable hotkeys help widget for VIM and other apps
--- when client with a matching name is opened:
--- require("awful.hotkeys_popup.keys")
-
--- {{{ Error handling
--- Check if awesome encountered an error during startup and fell back to
--- another config (This code will only ever execute for the fallback config)
+-- if there are errors during startup, use the default config
 if awesome.startup_errors then
-    naughty.notify({ preset = naughty.config.presets.critical,
-                     title = "Oops, there were errors during startup!",
-                     text = awesome.startup_errors })
+    naughty.notify {
+        preset = naughty.config.presets.critical,
+        title  = "Oops, there were errors during startup!",
+        text   = awesome.startup_errors
+    }
 end
 
--- Handle runtime errors after startup
+-- handle runtime errors after startup
 do
     local in_error = false
     awesome.connect_signal("debug::error", function (err)
-        -- Make sure we don't go into an endless error loop
-        if in_error then return end
+        -- make sure we don't go into an endless error loop
+        if in_error then
+            return
+        end
         in_error = true
 
-        naughty.notify({ preset = naughty.config.presets.critical,
-                         title = "Oops, an error happened!",
-                         text = tostring(err) })
+        naughty.notify {
+            preset = naughty.config.presets.critical,
+            title  = "Oops, an error happened!",
+            text   = tostring(err)
+        }
         in_error = false
     end)
 end
--- }}}
 
--- {{{ Variable definitions
--- Themes define colours, icons, font and wallpapers.
--- beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
-beautiful.init("/home/marko/.config/awesome/theme.lua")
 
--- Autostart script
-awful.spawn.once(util_dir .. "autostart.sh")
 
--- This is used later as the default terminal and editor to run.
-terminal = "alacritty"
-editor = os.getenv("EDITOR") or "nano"
+-- set default variables
+terminal   = "alacritty"
+editor     = os.getenv "editor" or "vi"
 editor_cmd = terminal .. " -e " .. editor
+utils      = gears.filestystem.get_configuration_dir() .. "util/"
+scripts    = utils .. "scripts/"
 
--- Default modkey
-modkey = "Mod4"
+-- load the theme file
+beautiful.init(
+    gears.filesystem.get_configuration_dir() .. "theme.lua"
+)
 
--- Table of layouts to cover with awful.layout.inc, order matters.
+-- execute the autostart script
+awful.spawn.once(
+    scripts .. "/autostart.sh"
+)
+
+-- select layouts
 awful.layout.layouts = {
     awful.layout.suit.tile,
     awful.layout.suit.fair,
@@ -78,107 +80,3 @@ awful.layout.layouts = {
     -- awful.layout.suit.corner.sw,
     -- awful.layout.suit.corner.se,
 }
--- }}}
-
--- Load the bar
-require('bar')
-
--- Load keybindings
-require('mappings')
-root.keys(globalkeys)
-
--- Load titlebars
--- require("titlebar")
-
--- Rules to apply to new clients (through the "manage" signal).
-awful.rules.rules = {
-    -- All clients will match this rule.
-    { rule = { },
-      properties = { border_width = beautiful.border_width,
-                     border_color = beautiful.border_normal,
-                     focus = awful.client.focus.filter,
-                     raise = true,
-                     keys = clientkeys,
-                     buttons = clientbuttons,
-                     screen = awful.screen.preferred,
-                     placement = awful.placement.no_overlap+awful.placement.no_offscreen }
-    },
-    -- Floating clients.
-    { rule_any = {
-        instance = {
-          -- "DTA",  -- Firefox addon DownThemAll.
-          "copyq",  -- Includes session name in class.
-          "pinentry",
-        },
-        class = {
-          "Arandr",
-		  "iwgtk",
-          -- "Blueman-manager",
-          -- "Gpick",
-          -- "Galculator",
-		  "gnome-calculator",
-		  "Iwgtk",
-          -- "Kruler",
-          -- "MessageWin",  -- kalarm.
-          -- "Sxiv",
-          "LibreOffice",
-          -- "Wpa_gui",
-          -- "Pcmanfm",
-		  -- "Thunar",
-          -- "veromix",
-          -- "xtightvncviewer"
-        },
-        name = {
-          "Event Tester",  -- xev.
-        },
-        role = {
-          -- "AlarmWindow",  -- Thunderbird's calendar.
-          -- "ConfigManager",  -- Thunderbird's about:config.
-          "pop-up",       -- e.g. Google Chrome's (detached) Developer Tools.
-        }
-      }, properties = { floating = true }},
-
-	-- { rule_any = {
-	-- 	type = {
-	-- 		"normal",
-	-- 		"dialog"
-	-- 	}
-	-- 	}, properties = { titlebars_enabled = true }
-	-- },
-}
-
--- Signals
-
--- Signal function to execute when a new client appears.
-client.connect_signal("manage", function (c)
-    -- Set the windows at the slave,
-    -- i.e. put it at the end of others instead of setting it master.
-    if not awesome.startup then awful.client.setslave(c) end
-
-    if awesome.startup
-      and not c.size_hints.user_position
-      and not c.size_hints.program_position then
-        -- Prevent clients from being unreachable after screen count changes.
-        awful.placement.no_offscreen(c)
-    end
-
-	-- Rounded corners
-	--[[ c.shape = function(cr,w,h)
- 		require("gears").shape.rounded_rect(cr,w,h,10)
-	end ]]
-end)
-
-client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
-client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
-screen.connect_signal("arrange", function (s)
-    local max = s.selected_tag.layout.name == "max"
-    local only_one = #s.tiled_clients == 1 -- use tiled_clients so that other floating windows don't affect the count
-    -- but iterate over clients instead of tiled_clients as tiled_clients doesn't include maximized windows
-    for _, c in pairs(s.clients) do
-        if (max or only_one) and not c.floating or c.maximized then
-            c.border_width = 0
-        else
-            c.border_width = beautiful.border_width
-        end
-    end
-end)
